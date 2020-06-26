85cf369b4a75f648b9f2a276ff8b2c035f081824
Joe Brimeyer <jbrimeyer@gmail.com>
2020-06-25 20:25:57 -0700

    added post to json functionality.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,21 +3,39 @@ const notesInfo = require("../db/db.json");
 
 module.exports = function (app) {
 
-    app.delete("/api/notes/:id", function (req, res) {
-        let id = req.params.id;
-        fs.readFileSync("./db/db.json", JSON.stringify(oldNotes));
-        // req.params.id
+    function writeNote(notes) {
+        notes = JSON.stringify(notes);
+        console.log(notes);
+        fs.writeFileSync("./db/b.json", notes, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
     }
-    )
+    // the GET api
+    app.get("/api/notes", function (req, res) {
+        res.json(notesInfo);
+        console.log(notesInfo);
+    });
+// the POST api
     app.post("/api/notes", function (req, res) {
         let newNote = req.body;
+        console.log(newNote);
         newNote.id = Math.floor(Math.random() * 100000);
 
-        const oldNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+        let oldNotes = JSON.parse(fs.readFileSync("./db/db.json"));
         oldNotes.push(newNote);
 
         let status = fs.writeFileSync("./db/db.json", JSON.stringify(oldNotes));
-        res.JSON(newNote);
-    })
+        res.json(newNote);
+    });
+    
+    // app.delete("/api/notes/:id", function (req, res) {
+    //     let id = req.params.id;
+    //     fs.readFileSync("./db/db.json", JSON.stringify(oldNotes));
+    //     // code here...
+    // });
+    
+    
 
 }
